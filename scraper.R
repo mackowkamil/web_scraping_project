43d3cbd6e9292cdd1701a2c69c14b2c1942fdc0a
mackowkamil <276913@student.pwr.edu.pl>
2024-01-31 19:19:45 +0100

    add ingredients scraping

--- scraper.R
+++ scraper.R
@@ -3,7 +3,8 @@ library(httr)
 
 links <- list()
 
-for (i in 115:121) {
+# first get all the URLs redirecting to the main dishes 
+for (i in 1:10) {
   url <- sprintf("https://kuchnialidla.pl/przepisy/dania-glowne/%d#lista", i)
   tryCatch(
     {
@@ -21,3 +22,35 @@ for (i in 115:121) {
 
 links <- unique(links)
 links <- paste0("https://kuchnialidla.pl", links, sep='')
+
+# create an empty data frame to store all the recipes info
+recipes <- data.frame(
+  Name = character(),
+  Ingredients = I(list())
+  stringsAsFactors = FALSE
+)
+
+# iterate through the links 
+# for (i in 1:length(links)) {
+for (i in 1:5) {
+  title <-  sub(".*/([^/]+)$", "\\1", links[i])
+  tryCatch(
+    {
+      recipe <- GET(links[1], user_agent('Hi it is a student project')) %>% content()
+      node <- html_element(recipe, xpath= '//*[@id="details"]/div[2]/div[1]')
+      ingred <- html_elements(node, 'li') %>% html_text()
+      ingred <- sub("\\s*-.*", "", ingred) 
+    },
+    error = function(e) e
+  )
+  new_row <- data.frame(
+    Name = title,
+    Ingredients = ingred
+    stringsAsFactors = FALSE
+  )
+  recipes <- rbind(recipes, new_row)
+}
+
+
+
+
