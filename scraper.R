910f83c9f9e61a1dcc6db99b3476edab4c15a6d0
mackowkamil <276913@student.pwr.edu.pl>
2024-01-31 20:04:55 +0100

    add macro scraping

--- scraper.R
+++ scraper.R
@@ -26,7 +26,12 @@ links <- paste0("https://kuchnialidla.pl", links, sep='')
 # create an empty data frame to store all the recipes info
 recipes <- data.frame(
   Name = character(),
-  Ingredients = I(list())
+  Ingredients = I(list()),
+  Energy_kcal = character(),
+  Fat_g = character(),
+  Carbohydrates_g = character(),
+  Fiber_g = character(),
+  Protein_g = character(),
   stringsAsFactors = FALSE
 )
 
@@ -34,18 +39,28 @@ recipes <- data.frame(
 # for (i in 1:length(links)) {
 for (i in 1:5) {
   title <-  sub(".*/([^/]+)$", "\\1", links[i])
-  tryCatch(
-    {
-      recipe <- GET(links[1], user_agent('Hi it is a student project')) %>% content()
-      node <- html_element(recipe, xpath= '//*[@id="details"]/div[2]/div[1]')
-      ingred <- html_elements(node, 'li') %>% html_text()
-      ingred <- sub("\\s*-.*", "", ingred) 
-    },
-    error = function(e) e
-  )
+  recipe <- GET(links[i], user_agent('Hi it is a student project')) %>% content()
+  
+  # get ingredients 
+  node <- html_element(recipe, xpath= '//*[@id="details"]/div[2]/div[1]')
+  ingred <- html_elements(node, 'li') %>% html_text()
+  ingred <- sub("\\s*-.*", "", ingred)
+  ingred_list <- list(ingred)
+  
+  # get dietary information
+  table <- html_element(recipe, xpath= '//*[@id="details"]/div[2]/div[2]/table') %>% html_table()
+  macro <- table[,2,2]
+  macro <- sub("\\s*[a-zA-Z]+$", "", macro[-1])
+  
+  # add new recipe to the data frame    
   new_row <- data.frame(
     Name = title,
-    Ingredients = ingred
+    Ingredients = I(ingred_list),
+    Energy_kcal = macro[1],
+    Fat_g = macro[2],
+    Carbohydrates_g = macro[3],
+    Fiber_g = macro[4],
+    Protein_g = macro[5],
     stringsAsFactors = FALSE
   )
   recipes <- rbind(recipes, new_row)
